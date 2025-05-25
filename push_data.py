2b4d9edf8320b697499ec77449ab5c6fe0e5b736
Sid-demo <sidsharma2401@gmail.com>
2025-05-25 17:34:33 +0530

    Data Inserted into MongoDB

---
+++ push_data.py
@@ -0,0 +1,61 @@
+import os 
+import sys 
+import json
+import certifi
+import pandas as pd
+import numpy as np 
+import pymongo
+import pymongo.mongo_client
+from NetworkSecurity.exception.exception import NetworkSecurityException
+from NetworkSecurity.logging import logger
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+
+print(MONGO_DB_URL)
+
+ca = certifi.where()
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def csv_to_json(self,file_path):
+        try:
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e :
+            raise NetworkSecurityException(e,sys)
+
+    def insert_data_mongo(self,records,database,collection):
+        try:  
+             self.records = records
+             self.database = database
+             self.collection = collection
+             self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+
+             self.database = self.mongo_client[self.database]
+             self.collection = self.database[self.collection]
+
+             self.collection.insert_many(self.records)
+             return len(self.records)
+        
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+
+if __name__== '__main__':
+    FILE_PATH = "NetworkData\phisingData.csv"
+    obj = NetworkDataExtract()
+    records = obj.csv_to_json(file_path=FILE_PATH)
+    print(records)
+    no_records = obj.insert_data_mongo(records=records,database="Siddhant",collection="NetworkData")    
+    print(no_records)
