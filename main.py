06cf695e9736c6a611767527aac817b7dbff538c
Sid-demo <sidsharma2401@gmail.com>
2025-05-31 20:02:11 +0530

    Data Transformation Compelete

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from NetworkSecurity.entity.config_entity import TrainingPipelineConfig
 from NetworkSecurity.entity.config_entity import DataValidationConfig
 from NetworkSecurity.components.data_validation import DataValidation
 from NetworkSecurity.entity.artifact_entity import DataValidationArtifact
+from NetworkSecurity.entity.config_entity import DataTransformationConfig
+from NetworkSecurity.entity.artifact_entity import DataTransformationArtifact
+from NetworkSecurity.components.data_transformation import DataTransformation
 import sys
 
 if __name__=="__main__":
@@ -24,6 +27,13 @@ if __name__=="__main__":
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
+
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
