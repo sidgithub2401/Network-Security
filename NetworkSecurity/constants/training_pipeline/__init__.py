663ebe56fa5e23b7e255ac5d63071f0984c12f00
Sid-demo <sidsharma2401@gmail.com>
2025-05-31 17:15:32 +0530

    Data Validation Completed

--- NetworkSecurity/constants/training_pipeline/__init__.py
+++ NetworkSecurity/constants/training_pipeline/__init__.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np 
 import pandas as pd 
 
+## Data Injestion Constants
 
 TARGET_COLUMN = "Result"
 PIPELINE_NAME : str = "NetworkSecurity"
@@ -10,6 +11,8 @@ ARTIFACT_DIR:str= "Artifacts"
 FILE_NAME :str="phisingData.csv"
 TRAIN_FILE_NAME:str= "train.csv"
 TEST_FILE_NAME:str= "test.csv"
+
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
 
  ## Data Injestion Constants
 
@@ -19,3 +22,12 @@ DATA_INJESTION_DIR_NAME:str = "data_injestion"
 DATA_INJESTION_FEATURE_STORE_DIR:str="feature_store"
 DATA_INJESTION_INJESTED_DIR:str="injested"
 DATA_INJESTION_TRAIN_TEST_SPLIT_RATIO:float= 0.2
+
+## Data Validation Constants 
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from NetworkSecurity.exception.exception import NetworkSecurityException
 from NetworkSecurity.logging.logger import logging
 from NetworkSecurity.entity.config_entity import DataInjestionConfig
 from NetworkSecurity.entity.config_entity import TrainingPipelineConfig
+from NetworkSecurity.entity.config_entity import DataValidationConfig
+from NetworkSecurity.components.data_validation import DataValidation
+from NetworkSecurity.entity.artifact_entity import DataValidationArtifact
 import sys
 
 if __name__=="__main__":
@@ -14,5 +17,13 @@ if __name__=="__main__":
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
